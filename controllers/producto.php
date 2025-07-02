8960b2036627f0fa985c7bfd719f219dc0857f1a
Allisson <allissonortega04@hotmail.com>
2025-07-02 01:00:23 -0500

    Add: control de codigo repetido en controller

--- controllers/producto.php
+++ controllers/producto.php
@@ -32,19 +32,25 @@ class ProductoController
 
     public function crear()
     {
-        $data = json_decode(file_get_contents("php://input"), true);
-        $stmt = $this->pdo->prepare("INSERT INTO productos (Codigo, Producto, Precio, Cantidad) VALUES (?, ?, ?, ?)");
-        $success = $stmt->execute([
-            $data['Codigo'],
-            $data['Producto'],
-            $data['Precio'],
-            $data['Cantidad']
-        ]);
-        echo json_encode(["success" => $success]);
+        
+         // Verificar si ya existe otro producto con el mismo código
+    $stmt = $this->pdo->prepare("SELECT id FROM productos WHERE Codigo = ? AND id != ?");
+    $stmt->execute([$data['Codigo'], $data['id']]);
+    
+        if ($stmt->rowCount() > 0) {
+            // Ya existe otro producto con ese código (distinto ID)
+            echo json_encode([
+                "success" => false,
+                "error" => "El código ya está en uso por otro producto."
+            ]);
+            return;
+        
+        }
     }
 
     public function actualizar($data)
     {
+       
         $stmt = $this->pdo->prepare("UPDATE productos SET Codigo=?, Producto=?, Precio=?, Cantidad=? WHERE id=?");
         $success = $stmt->execute([
             $data['Codigo'],
